5c8bd50ab7c2c004d4592dafe9b798e223860469
Justin Moore <justin.moore@sensiml.com>
2021-06-08 15:14:13 -0700

    Add Simple Streaming specification using v2 protocol.

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -30,6 +30,10 @@
 #include "sensor.h"
 #include "app_config.h"
 
+#if SENSIML_SIMPLE_STREAM_BUILD
+#include "ssi_comms.h"
+#endif //SENSIML_SIMPLE_STREAM_BUILD
+
 #define SYSTICK_FREQ_IN_MHZ 48 // !NB! This must be changed if the processor clock changes
 
 // *****************************************************************************
@@ -43,10 +47,29 @@ static volatile uint32_t tickcounter = 0;
 static struct sensor_device_t sensor;
 static struct sensor_buffer_t snsr_buffer;
 
+#if SENSIML_SIMPLE_STREAM_BUILD
+static SNSR_DATA_TYPE packet_data_buffer[SNSR_NUM_AXES * SNSR_SAMPLES_PER_PACKET];
+#define PACKET_BUFFER_BYTE_LEN (SNSR_NUM_AXES * SNSR_SAMPLES_PER_PACKET * sizeof(SNSR_DATA_TYPE))
+static int num_packets = 0;
+static void send_json_config()
+{
+    printf("{\"version\":%d, \"sample_rate\":%d,"
+    "\"samples_per_packet\":%d,"
+    "\"column_location\":{"
+    "\"AccelerometerX\":0,"
+    "\"AccelerometerY\":1,"
+    "\"AccelerometerZ\":2,"
+    "\"GyroscopeX\":3,"
+    "\"GyroscopeY\":4,"
+    "\"GyroscopeZ\":5}}\n", SSI_JSON_CONFIG_VERSION, SNSR_SAMPLE_RATE, SNSR_SAMPLES_PER_PACKET);
+}
+
+#endif //SENSIML_SIMPLE_STREAM_BUILD
+
 void SYSTICK_Callback(uintptr_t context) {
     static int mstick = 0;
     int tickrate = *((int *) context);
-    
+
     ++tickcounter;
     if (tickrate == 0) {
         mstick = 0;
@@ -83,53 +106,70 @@ void sleep_us(uint32_t us) {
 // For handling read of the sensor data
 void SNSR_ISR_HANDLER(uintptr_t context) {
     struct sensor_device_t *sensor = (struct sensor_device_t *) context;
-    
+
     /* Check if any errors we've flagged have been acknowledged */
     if (sensor->status != SNSR_STATUS_OK) {
         return;
     }
-    
+
     sensor->status = sensor_read(sensor, &snsr_buffer);
 }
 
 int main ( void )
 {
     int tickrate = 0;
-    
+
     /* Initialize all modules */
     SYS_Initialize ( NULL );
-    
+
     /* Register and start the LED ticker */
     SYSTICK_TimerCallbackSet(SYSTICK_Callback, (uintptr_t) &tickrate);
     SYSTICK_TimerStart();
-    
+
     /* Activate External Interrupt Controller for sensor capture */
     EIC_CallbackRegister(MIKRO_EIC_PIN, SNSR_ISR_HANDLER, (uintptr_t) &sensor);
     EIC_InterruptEnable(MIKRO_EIC_PIN);
-       
+
     /* Initialize our data buffer */
     buffer_init(&snsr_buffer);
-    
+
+#if SENSIML_SIMPLE_STREAM_BUILD
+    ssi_init(SERCOM5_USART_Read, SERCOM5_USART_Write);
+    send_json_config();
+
+    while(true)
+    {
+        ssi_try_connect();
+        if(ssi_connected())
+        {
+            break;
+        }
+        sleep_ms(1000);
+        send_json_config();
+    }
+#endif //SENSIML_SIMPLE_STREAM_BUILD
+
+
     printf("\r\n");
-    
+
     while (1)
-    {    
+    {
         if (sensor_init(&sensor) != SNSR_STATUS_OK) {
             printf("sensor init result = %d\r\n", sensor.status);
             break;
         }
-        
+
         if (sensor_set_config(&sensor) != SNSR_STATUS_OK) {
             printf("sensor configuration result = %d\r\n", sensor.status);
             break;
         }
-        
+
         printf("sensor sample rate set at %dHz\r\n", SNSR_SAMPLE_RATE);
         tickrate = TICK_RATE_SLOW;
-        
+
         break;
     }
-    
+
     while (1)
     {
         if (sensor.status != SNSR_STATUS_OK) {
@@ -138,30 +178,41 @@ int main ( void )
         }
         else if (snsr_buffer.overrun == true) {
             printf("\r\n\r\n\r\nOverrun!\r\n\r\n\r\n");
-            
+
             // Light the LEDs to indicate overflow
             tickrate = 0;
             LED_YELLOW_On();  // Indicate OVERFLOW
             sleep_ms(5000U);
             LED_YELLOW_Off(); // Clear OVERFLOW
             tickrate = TICK_RATE_SLOW;
-            
+
             buffer_reset(&snsr_buffer);
             continue;
-        }  
+        }
         else {
             // Feed temp buffer
             buffer_data_t *ptr;
             int rdcnt = buffer_get_read_buffer(&snsr_buffer, &ptr);
 
             while ( --rdcnt >= 0 ) {
-#if DATA_VISUALIZER_BUILD
+#if SENSIML_SIMPLE_STREAM_BUILD
+                for(int i = 0; i < SNSR_NUM_AXES; i++)
+                {
+                    packet_data_buffer[i]=ptr[i];
+                }
+                num_packets++;
+                if(num_packets == SNSR_SAMPLES_PER_PACKET)
+                {
+                    ssiv2_publish_sensor_data(0, (uint8_t*) packet_data_buffer, PACKET_BUFFER_BYTE_LEN);
+                    num_packets = 0;
+                }
+#elif DATA_VISUALIZER_BUILD
                 uint8_t headerbyte = MPDV_START_OF_FRAME;
-                
+
                 SERCOM5_USART_Write(&headerbyte, 1);
-                
+
                 SERCOM5_USART_Write(ptr, SNSR_NUM_AXES*sizeof(buffer_data_t));
-                
+
                 headerbyte = ~headerbyte;
                 SERCOM5_USART_Write(&headerbyte, 1);
                 headerbyte = ~headerbyte;
@@ -176,13 +227,13 @@ int main ( void )
                 buffer_advance_read_index(&snsr_buffer, 1);
             }
         }
-        
+
     }
-    
+
     tickrate = 0;
     LED_GREEN_Off();
     LED_RED_On();
-    
+
     return ( EXIT_FAILURE );
 }
 
---
+++ firmware/src/ssi_comms.c
@@ -0,0 +1,138 @@
+/** @file ssi_comms.c */
+
+/*==========================================================
+ * Copyright 2021 QuickLogic Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *==========================================================*/
+#include "app_config.h"
+#if SENSIML_SIMPLE_STREAM_BUILD
+#include <inttypes.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "ssi_comms.h"
+
+static uint32_t ssi_conn_seqnum[SSI_MAX_CHANNELS] = {0};
+#define CONNECT_STRING "connect"
+#define CONNECT_CHARS 7
+#define DISCONNECT_STRING "disconnect"
+#define DISCONNECT_CHARS 10
+
+static char connection_buf[DISCONNECT_CHARS];
+
+static ssi_io_funcs_t ssi_interface;
+
+
+
+void ssi_init(bool (*read)(void*, const size_t),  bool (*write)(void*, const size_t))
+{
+    ssi_interface.ssi_read = read;
+    ssi_interface.ssi_write = write;
+    ssi_interface.initialized = true;
+    ssi_interface.connected = false;
+}
+
+bool ssi_connected(void)
+{
+    return ssi_interface.initialized && ssi_interface.connected;
+}
+
+void ssi_try_connect(void)
+{
+    if(ssi_interface.ssi_read((uint8_t*)connection_buf, CONNECT_CHARS))
+    {
+        if(strncmp(connection_buf, CONNECT_STRING, CONNECT_CHARS))
+        {
+            ssi_interface.connected = true;
+        }
+    }
+}
+
+void ssi_seqnum_init(uint8_t channel)
+{
+    if (channel >= SSI_MAX_CHANNELS)
+      return;
+    ssi_conn_seqnum[channel] = 0;
+}
+
+void ssi_seqnum_reset(uint8_t channel)
+{
+    if (channel >= SSI_MAX_CHANNELS)
+      return;
+    ssi_conn_seqnum[channel] = 0;
+}
+
+uint32_t ssi_seqnum_update(uint8_t channel)
+{
+    if (channel >= SSI_MAX_CHANNELS)
+      return 0;
+    ssi_conn_seqnum[channel]++;
+    return ssi_conn_seqnum[channel];
+}
+
+uint32_t ssi_seqnum_get(uint8_t channel)
+{
+    if (channel >= SSI_MAX_CHANNELS)
+      return 0;
+    return ssi_conn_seqnum[channel];
+}
+
+uint8_t ssi_payload_checksum_get(uint8_t *p_data, uint16_t len)
+{
+    uint8_t crc8 = p_data[0];
+    for (int k = 1; k < len; k++)
+    {
+        crc8 ^= p_data[k];
+    }
+    return crc8;
+}
+
+void ssiv2_publish_sensor_data(uint8_t channel, uint8_t* buffer, int size)
+{
+    if(ssi_interface.initialized == false)
+    {
+        return;
+    }
+    uint8_t ssiv2header[SSI_HEADER_SIZE];
+    uint8_t sync = SSI_SYNC_DATA;
+    uint8_t rsvd = 0;
+    uint16_t u16len = (size + 6);
+    uint32_t seqnum = ssi_seqnum_update(channel);
+    uint8_t crc8 = 0;
+
+    ssiv2header[0] = sync;
+    ssiv2header[1] = (u16len >> 0) & 0xff;
+    ssiv2header[2] = (u16len >> 8) & 0xff;
+    ssiv2header[3] = rsvd;
+    ssiv2header[4] = channel;
+    ssiv2header[5] = (seqnum >> 0) & 0xff;
+    ssiv2header[6] = (seqnum >> 8) & 0xff;
+    ssiv2header[7] = (seqnum >> 16) & 0xff;
+    ssiv2header[8] = (seqnum >> 24) & 0xff;
+
+    // compute 8-bit checksum
+    crc8 = crc8 ^ ssi_payload_checksum_get(ssiv2header+3, SSI_HEADER_SIZE-3);
+    crc8 = crc8 ^ ssi_payload_checksum_get(buffer, size);
+
+    // Send SSI v2 header information
+    ssi_interface.ssi_write(ssiv2header, SSI_HEADER_SIZE);
+
+    // Send sensor data
+    ssi_interface.ssi_write(buffer, size);
+
+    // Add 8-bit checksum
+    ssi_interface.ssi_write(&crc8, 1);
+}
+
+#endif //SENSIML_SIMPLE_STREAM_BUILD
